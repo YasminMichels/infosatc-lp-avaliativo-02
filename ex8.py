d1a842f07f2726b2bde1258e08ddf1e6b9060bbb
Yasmin Michels <yamichels@gmail.com>
2021-09-09 16:38:00 -0300

    ex8
    exercicio de numero 8

---
+++ ex8.py
@@ -0,0 +1,7 @@
+pcgamer = ["Intel Core i5 10400"," HD de 1 TB e 8 GB","Asus EX-B460M-V5","Monitor Samsung 144 Hertz","Mouse Gamer RGB Alienware"]
+pcgamer.append("Gabinete cooler master")
+pcgamer.append("GeForce RTX 2080Ti")
+print("Itens para o pc gamer: ", pcgamer)
+pcgamer.pop()
+pcgamer.pop()
+print("Itens para o pc gamer dentro do orçamento: ",pcgamer)
