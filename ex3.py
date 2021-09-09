d07bf9607e91e43683cd80c6a146c83c29407e45
Yasmin Michels <yamichels@gmail.com>
2021-09-09 15:44:02 -0300

    ex3
    exercicio de numero 3

---
+++ ex3.py
@@ -0,0 +1,8 @@
+n1 = int(input("Digite um numero: "))
+n2 = int(input("Digite um numero: "))
+n3 = int(input("Digite um numero: "))
+n4 = int(input("Digite um numero: "))
+n5 = int(input("Digite um numero: "))
+
+lista = [n1,n2,n3,n4,n5]
+print("A soma dos números resulta em: ",sum(lista))
