708b3f0d4be613694b717185de8d3d6ec0746f7b
Yasmin Michels <yamichels@gmail.com>
2021-09-09 15:55:53 -0300

    ex6
    exercicio de numero 6

---
+++ ex6.py
@@ -0,0 +1,6 @@
+listacor = ["vermelho","roxo","preto","azul"]
+print(listacor)
+print("O item vermelho está na posição:",listacor.index("vermelho"))
+print("O item roxo está na posição:",listacor.index("roxo"))
+print("O item preto está na posição:",listacor.index("preto"))
+print("O item azul está na posição:",listacor.index("azul"))
